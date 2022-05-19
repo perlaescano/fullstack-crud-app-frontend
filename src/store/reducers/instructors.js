b1c952cce6fd4192decfaa53968587bfe8d41659
perlaescano <perlaescanoe@gmail.com>
2022-05-19 14:02:22 -0400

    updated instructors reducer

--- src/store/reducers/instructors.js
+++ src/store/reducers/instructors.js
@@ -2,12 +2,22 @@ import * as at from "../actions/actionTypes";
 
 // REDUCER;
 const allInstructors = (state = [], action) => {
+
   switch (action.type) {
     case at.FETCH_ALL_INSTRUCTORS:
-      return action.payload;
+        return action.payload;
+    case at.ADD_INSTRUCTOR:
+        return [...state, action.payload];
+    case at.DELETE_INSTRUCTOR:
+        return state.filter(instructor => instructor.id !== action.payload);
+    case at.EDIT_INSTRUCTOR:
+        return state.map(instructor =>
+            instructor.id === action.payload.id ? action.payload : instructor
+        );
+    // might need add, edit, delete student cases
     default:
-      return state;
-  }
+        return state;
+}
 };
 
-export default allInstructors;
+export default allInstructors; 
