3ec5d1745a82161cf8ba64c1ce395580a726d928
perlaescano <perlaescanoe@gmail.com>
2022-05-19 12:07:42 -0400

    allcourseview format

--- src/components/views/AllCoursesView.js
+++ src/components/views/AllCoursesView.js
@@ -1,41 +1,71 @@
+import PropTypes from "prop-types";
+import {useStyles} from "../Styles";
+import NavigableContainer from "../containers/NavigableContainer";
+import useWindowDimensions from "../../utils/WindowDimensions";
+import {CourseCard} from "../cards/CourseCard";
+import {Button, Card, CardActions} from '@mui/material';
+import {Link} from "react-router-dom";
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
-   
-import { Link } from "react-router-dom";
+const CARD_HEIGHT = 130;
+const CARD_WIDTH = 1.7125 * CARD_HEIGHT;
 
-const AllCoursesView = (props) => {
-  let {courses, deleteCourse} = props;
-  //courses = [{id: 300, title: "hello"}]
-  if (!courses.length) {
-    return (
-    <div>
-      <p>There are no courses.</p>
-      <Link to={`/newcourse`}>
-        <button>Add New Course</button>
-      </Link>
-    </div>
+const NewCourse = () => {
+  return (
+    <Card raised>
+            <CardActions>
+                <Link to={`newcourse`}>
+                    <Button aria-label="Add New Course" startIcon={<AddCircleIcon/>}>
+                        Add New Course
+                    </Button>
+                </Link>
+            </CardActions>
+        </Card>
     );
   }
+
+  const AllCoursesView = ({ courses, deleteCourse }) => {
+      const classes = useStyles();
+      const { width: pageWidth } = useWindowDimensions();
+      const numColumns = Math.round(Math.max(pageWidth / CARD_WIDTH, 1));
   
-  return (
-    <div>
-      {courses.map((course) => {
-        let title = course.title;
-        return (
-          <div key={course.id}>
-          <Link to={`/course/${course.id}`}>
-            <h1>{title}</h1>
-          </Link>
-          <button onClick={() => deleteCourse(course.id)}>Delete</button>
-          </div>
+      if (!courses.length) {
+          return (
+            <NavigableContainer classes={classes}>
+                <p>There are no courses.</p>
+                <div style={{
+                    display: "inline-grid",
+                    gridTemplateColumns: `repeat(${numColumns}, 1fr)`,
+                    gridGap: "10px",
+                }}>
+                    <NewCourse/>
+                </div>
+            </NavigableContainer>
         );
       }
-      )}
-      <Link to={`/newcourse`}>
-        <button>Add New Course</button>
-      </Link>
-    </div>
-  );
-};
 
-
-export default AllCoursesView;
+      return (
+          <NavigableContainer classes={classes}>
+              <div style={{
+                  display: "inline-grid",
+                  gridTemplateColumns: `repeat(${numColumns}, 1fr)`,
+                  gridGap: "10px",
+              }}>
+                  <NewCourse classes={classes}/>
+                  {courses.map(course => <CourseCard
+                      key={course.id}
+                      classes={classes}
+                      deleteFn={deleteCourse}
+                      cardHeight={CARD_HEIGHT}
+                      object={course}/>)}
+              </div>
+          </NavigableContainer>
+      );
+  };
+  
+  AllCoursesView.propTypes = {
+      courses: PropTypes.array.isRequired,
+      deleteCourse: PropTypes.func.isRequired,
+  };
+  
+  
