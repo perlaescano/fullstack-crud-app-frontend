66a03b56af4a96e2d2ea6c79e881f037aad50180
perlaescano <perlaescanoe@gmail.com>
2022-05-19 13:48:14 -0400

    now exports add_intructor, delete_instructor and edit_instructor actions

--- src/components/views/CourseView.js
+++ src/components/views/CourseView.js
@@ -1,16 +1,35 @@
-import { Link } from "react-router-dom";
-import { Button } from "@material-ui/core";
-const CourseView = (props) => {
-  const { course } = props;
-  return (
-    <div>
-      <h1>{course.title}</h1>
-      <h3>{course.timeslot}</h3>
-      <h3>{course.location}</h3>
-      <h3>{course.instructor.firstName + " " + course.instructor.lastName}</h3>
-    </div>
-  );
+import {useStyles} from "../Styles";
+import NavigableContainer from "../containers/NavigableContainer";
+import CourseForm from "../forms/CourseForm";
+import {CourseCard} from "../cards/CourseCard";
+import {InstructorCard} from "../cards/InstructorCard";
 
+const CourseView = ({ course, cancelForm, handleChange, handleSubmit }) => {
+  const classes = useStyles();
+  return (
+      <NavigableContainer course={course}>
+          <div style={{
+              display: "flex",
+              gridGap: "10px",
+          }}>
+              <div style={{
+                  display: "grid",
+                  height: "90vh",
+                  // flexWrap: "wrap",
+                  gridGap: "10px"
+              }}>
+                  <CourseCard course={course} object={course} cardHeight="auto" notClickable/>
+                  <InstructorCard course={course} object={course.instructor} cardHeight="auto"/>
+              </div>
+              <CourseForm
+                  cancelForm={cancelForm}
+                  handleChange={handleChange}
+                  handleSubmit={handleSubmit}
+                  course={course}
+              />
+          </div>
+      </NavigableContainer>
+  );
 };
 
 export default CourseView;
