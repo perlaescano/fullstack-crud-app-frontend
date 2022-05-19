ea1c2d5532d4f6e652d856f2e23a490d04740bb5
perlaescano <perlaescanoe@gmail.com>
2022-05-19 13:57:29 -0400

    added instructor reducer

--- src/store/reducers/instructor.js
+++ src/store/reducers/instructor.js
@@ -1,16 +1,53 @@
-import { FETCH_INSTRUCTOR } from "../actions/actionTypes";
+import {
+  ADD_COURSE,
+  DELETE_INSTRUCTOR,
+  DELETE_COURSE,
+  EDIT_INSTRUCTOR,
+  EDIT_COURSE,
+  FETCH_INSTRUCTOR
+} from "../actions/actionTypes";
+import courses from "./courses";
 
 const initialState = {
   courses: [],
 };
 
 const instructor = (state = initialState, action) => {
+
   switch (action.type) {
     case FETCH_INSTRUCTOR:
-      return action.payload;
+    case EDIT_INSTRUCTOR:
+        return action.payload;
+    case DELETE_INSTRUCTOR:
+        return initialState;
+    case ADD_COURSE:
+        return {
+            ...state,
+            courses: courses(state.courses, action.payload)
+        }
+    case DELETE_COURSE:
+        return {
+            ...state,
+            courses: courses(state.courses, action.payload)
+        }
+    case EDIT_COURSE:
+        if (state.courses.some(course => course.id === action.payload.id))
+            if (state.id !== action.payload.instructorId)
+                return instructor(state, {
+                    payload: action.payload.id,
+                    type: DELETE_COURSE });
+            else
+                return {
+                    ...state,
+                    courses: courses(state.courses, action)
+                };
+        else if (state.id === action.payload.instructorId)
+            return instructor(state, {...action, type: ADD_COURSE})
+        else
+            return state;
     default:
-      return state;
-  }
+        return state;
+}
 };
 
-export default instructor;
+export default instructor; 
