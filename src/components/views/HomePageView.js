d75b3192d268563694e692f94a15554242acd239
Eric Dittus <ericdittus@Erics-MacBook-Air.local>
2022-05-23 14:21:16 -0400

    made the navbar universal and edited the homeview

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -1,15 +1,28 @@
 
-import { useStyles } from '../Styles';
 import NavigableContainer from "../containers/NavigableContainer";
 import React from 'react';
+import hunter from "../img/hunter.jpg"
+
 
 const HomePageView = () => {
-  const courses = useStyles();
+
   return (
-    <NavigableContainer courses={courses}>
-      <div className={courses.info}><h1>Home Page</h1></div>
-      </NavigableContainer>
+
+      <div>
+        <div className="title" style = {title}>;
+          <h1 style = {{color: "white"}}>Final Project</h1>
+        </div>
+        <img src = {hunter} alt = "Hunter College"/>
+      </div>
+
   );
+}
+
+const title = {
+  justifyContent: 'center',
+  backgroundColor: '#7d0aff',
+  width: "100%",
+  margin: "auto",
 }
 
 
