4a1dbd00864808bd3792bcc8522c5fc5360d0652
Eric Dittus <ericdittus@Erics-MacBook-Air.local>
2022-05-23 00:16:21 -0400

    Worked on making the forms within the instructors and course view, i don't think we need a seperate form

--- src/components/views/NewCourseView.js
+++ src/components/views/NewCourseView.js
@@ -1,46 +1,86 @@
 import {makeStyles} from '@mui/styles';
-import NavigableContainer from "../containers/NavigableContainer";
-import CourseForm from "../forms/CourseForm";
+//import NavigableContainer from "../containers/NavigableContainer";
+//import CourseForm from "../forms/CourseForm";
+import Button from "@material-ui/core/Button";
 import React, { Component }  from 'react';
+import Typography from "@material-ui/core/Typography";
 
 
-const useStyles = makeStyles(() => ({
-    formContainer: {
-        width: '500px',
-        backgroundColor: '#61ffa8',
-        borderRadius: '5px',
-        margin: 'auto',
-    },
-    title: {
-        flexGrow: 1,
-        textAlign: 'left',
-        textDecoration: 'none'
-    },
-    customizeAppBar: {
-        backgroundColor: '#11153e',
-        shadows: ['none'],
-    },
-    formTitle: {
-        backgroundColor: '#261c69',
-        marginBottom: '15px',
-        textAlign: 'center',
-        borderRadius: '5px 5px 0px 0px',
-        padding: '3px'
-    },
-    labelStyle: {
-        color: '#9c40f7',
-        fontWeight: 'bold'
-    },
-}));
 
 const NewCourseView = ({handleChange, handleSubmit}) => {
 const courses = useStyles();
 return (
-  <NavigableContainer courses={courses}>
-      <CourseForm handleChange={handleChange} handleSubmit={handleSubmit}/>
-  </NavigableContainer>
+  <div>
+  <h1>New Course</h1>
+
+  <div className={courses.root}>
+    <div className={courses.formContainer}>
+      <div className={courses.formTitle}>
+        <Typography
+          style={{
+            fontWeight: "bold",
+            fontFamily: "Courier, sans-serif",
+            fontSize: "20px",
+            color: "#11153e",
+          }}
+        >
+          Add a Course
+        </Typography>
+      </div>
+      <form
+        style={{ textAlign: "center" }}
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <label style={{ color: "#11153e", fontWeight: "bold" }}>
+          Title:{" "}
+        </label>
+        <input
+          type="text"
+          name="Title"
+          onChange={(e) => handleChange(e)}
+          required
+        />
+        <br />
+        <br />
+
+        <label style={{ color: "#11153e", fontWeight: "bold" }}>
+          Timeslot:{" "}
+        </label>
+        <input
+          type="text"
+          name="Timeslot"
+          onChange={(e) => handleChange(e)}
+          required
+        />
+        <br />
+        <br />
+
+        <label style={{ color: "#11153e", fontWeight: "bold" }}>
+          Instructor ID:{" "}
+        </label>
+        <input
+          type="text"
+          name="Instructor ID"
+          onChange={(e) => handleChange(e)}
+          required
+        />
+        <br />
+        <br />
+
+        <Button variant="contained" color="primary" type="submit">
+          Submit
+        </Button>
+        <br />
+        <br />
+      </form>
+    </div>
+  </div>
+</div>
+
 );
 }
 
-
+const useStyles = makeStyles(() => ({
+    
+}));
 export default NewCourseView;
--- src/components/views/NewInstructorView.js
+++ src/components/views/NewInstructorView.js
@@ -1,46 +1,86 @@
 import {makeStyles} from '@mui/styles';
-import NavigableContainer from "../containers/NavigableContainer";
-import InstructorForm from "../forms/InstructorForm";
+//import NavigableContainer from "../containers/NavigableContainer";
+//import InstructorForm from "../forms/InstructorForm";
 import React, { Component }  from 'react';
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 
-const useStyles = makeStyles(() => ({
-    formContainer: {
-        width: '500px',
-        backgroundColor: '#61ffa8',
-        borderRadius: '5px',
-        margin: 'auto',
-    },
-    title: {
-        flexGrow: 1,
-        textAlign: 'left',
-        textDecoration: 'none'
-    },
-    customizeAppBar: {
-        backgroundColor: '#11153e',
-        shadows: ['none'],
-    },
-    formTitle: {
-        backgroundColor: '#261c69',
-        marginBottom: '15px',
-        textAlign: 'center',
-        borderRadius: '5px 5px 0px 0px',
-        padding: '3px'
-    },
-    labelStyle: {
-        color: '#9c40f7',
-        fontWeight: 'bold'
-    },
-}));
 
 const NewInstructorView = ({handleChange, handleSubmit}) => {
 const instructors = useStyles();
 return (
-  <NavigableContainer instructors={instructors}>
-      <InstructorForm handleChange={handleChange} handleSubmit={handleSubmit}/>
-  </NavigableContainer>
+  <div>
+        <h1>New Instructor</h1>
+
+        <div className={instructors.root}>
+          <div className={instructors.formContainer}>
+            <div className={instructors.formTitle}>
+              <Typography
+                style={{
+                  fontWeight: "bold",
+                  fontFamily: "Courier, sans-serif",
+                  fontSize: "20px",
+                  color: "#11153e",
+                }}
+              >
+                Add an Instructor
+              </Typography>
+            </div>
+            <form
+              style={{ textAlign: "center" }}
+              onSubmit={(e) => handleSubmit(e)}
+            >
+              <label style={{ color: "#11153e", fontWeight: "bold" }}>
+                First Name:{" "}
+              </label>
+              <input
+                type="text"
+                name="First Name"
+                onChange={(e) => handleChange(e)}
+                required
+              />
+              <br />
+              <br />
+
+              <label style={{ color: "#11153e", fontWeight: "bold" }}>
+                Last Name:{" "}
+              </label>
+              <input
+                type="text"
+                name="Last Name"
+                onChange={(e) => handleChange(e)}
+                required
+              />
+              <br />
+              <br />
+
+              <label style={{ color: "#11153e", fontWeight: "bold" }}>
+                Image URL:{" "}
+              </label>
+              <input
+                type="text"
+                name="ImageURL"
+                onChange={(e) => handleChange(e)}
+              />
+              <br />
+              <br />
+
+              <Button variant="contained" color="primary" type="submit">
+                Submit
+              </Button>
+              <br />
+              <br />
+            </form>
+          </div>
+        </div>
+      </div>
 );
 }
+
+const useStyles = makeStyles(() => ({
+    
+}));
 
 
 export default NewInstructorView;
